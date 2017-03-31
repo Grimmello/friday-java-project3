d2b47f32befb4ee47299d8550d6205e9c2778f8d
Clifford Grimmell <agrimmell2@gmail.com>
2017-03-31 08:51:15 -0700

    create stylist code and tests

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,9 +1,36 @@
 import org.sql2o.*;
+import java.util.*;
 
 public class Stylist {
+  private String stylistName;
+  private int id;
 
-  public Stylist() {
-
+  public Stylist(String name) {
+    this.stylistName = name;
   }
 
+  public String getStylistName() {
+    return stylistName;
+  }
+
+  public int getStylistId() {
+    return id;
+  }
+
+  public static List<Stylist> allStylists() {
+    String sql = "SELECT id, stylistName FROM stylists";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO stylists(stylistName) VALUES (:stylistName)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("stylistName", this.stylistName)
+        .executeUpdate()
+        .getKey();
+    }
+  }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -6,15 +6,33 @@ public class StylistTest {
 
   @Before
   public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/barber_shop_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
   }
 
   @After
   public void tearDown() {
     try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
+      String sql = "DELETE FROM stylists *;";
       con.createQuery(sql).executeUpdate();
     }
   }
 
+  @Test
+  public void newStylist_instanceOfStylist_true() {
+    Stylist newStylist = new Stylist("Jack");
+    assertEquals(true, newStylist instanceof Stylist);
+  }
+
+  @Test
+  public void newStylist_getStylistName_Jack() {
+    Stylist newStylist = new Stylist("Jack");
+    assertEquals("Jack", newStylist.getStylistName());
+  }
+
+  @Test
+  public void newStylist_getStylistName_Jack() {
+    Stylist newStylist = new Stylist("Jack");
+    newStylist.save();
+    assertEquals(true, );
+  }
 }
